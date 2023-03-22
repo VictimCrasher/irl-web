eeb473978965dabed35dc97e70685c6083a09604
VictimCrasher <achmadfirdausadinegoro@gmail.com>
2023-03-22 14:00:56 +0700

    Finishing hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,17 @@ const Hero = () => {
 	return (
 		<div className="hero">
 			<Spaces />
-			<div>Hero</div>
+			<div className="hero-flex">
+				<h1 className="super-big-headline">
+					Achmad <span className="hero-color">Firdaus</span> Adinegoro
+				</h1>
+				<h3 className="subheading">
+					Frontend Developer • AI Developer • Freelancer
+				</h3>
+				<button className="primary-btn">
+					More about me
+				</button>
+			</div>
 		</div>
 	);
 };
